60b210dcf5aea44fbad3743198518504865ebd32
Soulisse Baptiste <baptiste.soulisse@taodyne.com>
2011-12-25 11:37:24 +0100

    Merge branch 'master' of ssh://nano.taodyne.com/~ddd/Work/tao into merge

--- doc/digital_clock.doxy.h
+++ doc/digital_clock.doxy.h
@@ -1,5 +1,4 @@
 /**
- * @~english
  * @taomoduledescription{DigitalClock, Digital Clock}
  *
  * Shows a digital clock.
@@ -7,28 +6,13 @@
  * You can use the Digital Clock module in your presentation if you want
  * to display the time like a traditional alarm clock does.
  *
- * For example, the following document will show a nice clock
+ * For example, the following document
+ * (<a href="examples/digital_clock.ddd">digital_clock.ddd</a>) will show a nice clock
  * 40 pixels high:
  * @include digital_clock.ddd
  *
  * @image html digital_clock.png "Output of digital_clock.ddd"
  * @endtaomoduledescription{DigitalClock}
- *
- * @~french
- * @taomoduledescription{DigitalClock, Horloge Numérique}
- *
- * Affiche une horloge numérique.
- *
- * Ce module vous permet d'afficher l'heure dans votre présentation sous une
- * forme qui reseemble à une pendule numérique LCD ou à LED.
- *
- * Par exemple, le document qui suit affiche une horloge de 40 pixels de haut :
- * @include digital_clock.ddd
- *
- * @image html digital_clock.png "Une pendule numérique"
- * @endtaomoduledescription{DigitalClock}
- *
- * @~
  * @{
  */
 
@@ -39,75 +23,46 @@
 
 
 /**
- * @~english
- * The color for "ON" segments.
- * @~french
- * La couleur des segments allumés.
+ * The color for "ON" segments
  */
 text dc_segment_color_on  = "#229988";
 /**
- * @~english
- * The color for "OFF" segments.
- * @~french
- * La couleur des segments éteints.
+ * The color for "OFF" segments
  */
 text dc_segment_color_off = "#004477";
 /**
- * @~english
- * The display's background color.
- * @~french
- * La couleur du fond.
+ * The display's background color
  */
 text dc_background_color  = "#003366";
 /**
- * @~english
  * Whether or not to display 6 and 9 with a horizontal bar
  *   @li @c "off" Do not display a horizontal bar
  *   @li @c "on"  Display a horizontal bar
- * @~french
- * Permet d'afficher le 6 et le 9 avec ou sans barre horizontale.
- *   @li @c "off" N'affiche pas la barre horizontale
- *   @li @c "on"  Affiche la barre horizontale
+ * @bug Should be a boolean
  */
 text dc_hbar_6_9          = "off";
 /**
- * @~english
- * Whether or not to display the hour with a leading zero.
+ * Whether or not to display the hour with a leading zero
  *   @li @c "true"  Display a leading zero
  *   @li @c "false" Do not display a leading zero
- * @~french
- * Permet d'afficher ou non le premier zéro de l'heure.
- *   @li @c "true"  Affiche le zéro
- *   @li @c "false" N'affiche pas le zéro
  */
 text dc_hours_leading_zero = "false";
 /**
- * @~english
  * Whether or not the clock displays seconds
  *   @li @c "true"  Display hours, minutes and seconds
  *   @li @c "false" Display hours and minutes only
- * @~french
- * Permet d'afficher ou non les secondes.
- *   @li @c "true"  Affiche les heures, minutes et secondes.
- *   @li @c "false" Affiche les heures et minutes seulement.
  */
 text dc_show_seconds = "false";
 /**
- * @~english
  * The time to display
  * If @ref dc_time is -1.0, use current time. Otherwise, @ref dc_time is
- * the time to to display, expressed in seconds since Jan 1, 1970 (POSIX).
- * @~french
- * L'heure à afficher.
- * Si @ref dc_time vaut -1.0, l'heure courante est utilisée. Sinon,
- * @ref dc_time est l'heure à afficher, exprimée en secondes depuis le 1er
- * janvier 1970 (POSIX).
+ * to display, expressed in seconds as per POSIX time standards.
+ * This variable is of type real, so it can include a fractional part.
  */
 real dc_time = -1.0;
 
 
 /**
- * @~english
  * Shows a digital clock with specified size and position.
  * The clock is centered at (@a cX, @a cY) and z=0.
  * By default, it shows the current local time. You can display
@@ -135,47 +90,16 @@ real dc_time = -1.0;
  * @param cH the clock's height. The width is 2.86555 * @a cH when the
  *    clock shows only hours and minutes, and 4.5042 * @a cH when seconds
  *     are enabled.
- *
- * @~french
- * Affiche une horloge numérique.
- * L'horloge est centrée en (@a cX, @a cY) sur le plan z = 0.
- * Par défaut, elle affiche l'heure locale courante. Vous pouvez afficher
- * l'heure de n'importe quelle autre moment en définissant @ref dc_time. \n
- *
- * Les chiffres ont une forme similaire à ceux que l'on trouve sur certaines
- * horloges numériques à affichage LCD, VFD ou LCD : ils sont formés de sept
- * segments allumés ou éteints. La couleur de chaque état est configurable
- * grâce à @ref dc_segment_color_on et @ref dc_segment_color_off, la couleur
- * du fond étant @ref dc_background_color. \n
- *
- * L'horloge dispose d'autres options de configuration. Voyez la documentation
- * de @ref dc_hours_leading_zero, @ref dc_show_seconds et @ref dc_hbar_6_9. \n
- *
- * Seul le format d'affichage sur 24 heures est supporté.
- *
- * @param cX Abscisse du centre de l'horloge, en pixels.
- * @param cY Ordonnée du centre de l'horloge, en pixels.
- * @param cH Hauteur de l'horloge en pixels. La largeur est 2.86555 * @a cH
- *    lorsque l'heure et les minutes seulement sont affichées, et
- *    4.5042 * @a cH lorsque les secondes sont également affichéees.
  */
 digital_clock(real cX, real cY, real cH);
 
 /**
- * @~english
  * Shows a digital clock of height 238 px at (@a x, @a y).
- * @~french
- * Affiche une horloge numérique.
- * L'horloge est centrée en (@a x, @a y) et sa hauteur est 238 pixels.
  */
 digital_clock(real x, real y);
 
 /**
- * @~english
  * Shows a digital clock of height 238 px at (0, 0).
- * @~french
- * Affiche une horloge numérique au centre de l'écran.
- * L'horloge est centrée en (0, 0) et sa hauteur est 238 pixels.
  */
 digital_clock();
 
