6790d3f5220f9bd1afb54ff95105df736b004387
Catherine Burvelle <catherine@taodyne.com>
2013-08-12 12:16:41 +0200

    Merge branch 'master' of ssh://io.taodyne.com/~ddd/Work/tao

--- doc/digital_clock.doxy.h
+++ doc/digital_clock.doxy.h
@@ -1,8 +1,7 @@
 /**
  * @~english
  * @taomoduledescription{DigitalClock, Digital Clock}
- *
- * <tt>import DigitalClock</tt> - Shows a digital clock.
+ * <tt>import DigitalClock</tt> - Shows a digital clock.@n
  *
  * You can use the Digital Clock module in your presentation if you want
  * to display the time like a traditional alarm clock does.
@@ -16,8 +15,7 @@
  *
  * @~french
  * @taomoduledescription{DigitalClock, Horloge Numérique}
- *
- * <tt>import DigitalClock</tt> - Affiche une horloge numérique.
+ * <tt>import DigitalClock</tt> - Affiche une horloge numérique.@n
  *
  * Ce module vous permet d'afficher l'heure dans votre présentation sous une
  * forme qui ressemble à une pendule numérique LCD ou à LED.
